825ffd7cde0aef96c6d2319cf6377cb66c125ad7
ropr123 <m.rohit.prabhu@gmail.com>
2026-04-12 16:46:50 -0400

    fix: correct terraform variable syntax in new generic modules

--- terraform/modules/cloud-run-job/variables.tf
+++ terraform/modules/cloud-run-job/variables.tf
@@ -1,12 +1,34 @@
-variable "project_id" { type = string }
-variable "region" { type = string }
-variable "name" { type = string }
-variable "image" { type = string }
-variable "service_account_email" { type = string }
-variable "vpc_connector_id" { type = string; default = null }
-variable "db_instance_connection_name" { type = string; default = null }
-variable "command" { type = list(string); default = [] }
-variable "args" { type = list(string); default = [] }
+variable "project_id" {
+  type = string
+}
+variable "region" {
+  type = string
+}
+variable "name" {
+  type = string
+}
+variable "image" {
+  type = string
+}
+variable "service_account_email" {
+  type = string
+}
+variable "vpc_connector_id" {
+  type    = string
+  default = null
+}
+variable "db_instance_connection_name" {
+  type    = string
+  default = null
+}
+variable "command" {
+  type    = list(string)
+  default = []
+}
+variable "args" {
+  type    = list(string)
+  default = []
+}
 variable "env_vars" { 
   type = list(object({ name = string, value = string }))
   default = [] 
--- terraform/modules/cloud-run-service/variables.tf
+++ terraform/modules/cloud-run-service/variables.tf
@@ -1,16 +1,50 @@
-variable "project_id" { type = string }
-variable "region" { type = string }
-variable "name" { type = string }
-variable "image" { type = string }
-variable "container_port" { type = number; default = 8080 }
-variable "service_account_email" { type = string }
-variable "vpc_connector_id" { type = string; default = null }
-variable "db_instance_connection_name" { type = string; default = null }
-variable "command" { type = list(string); default = [] }
-variable "args" { type = list(string); default = [] }
-variable "cpu" { type = string; default = "1" }
-variable "memory" { type = string; default = "512Mi" }
-variable "cpu_idle" { type = bool; default = true }
+variable "project_id" {
+  type = string
+}
+variable "region" {
+  type = string
+}
+variable "name" {
+  type = string
+}
+variable "image" {
+  type = string
+}
+variable "container_port" {
+  type    = number
+  default = 8080
+}
+variable "service_account_email" {
+  type = string
+}
+variable "vpc_connector_id" {
+  type    = string
+  default = null
+}
+variable "db_instance_connection_name" {
+  type    = string
+  default = null
+}
+variable "command" {
+  type    = list(string)
+  default = []
+}
+variable "args" {
+  type    = list(string)
+  default = []
+}
+variable "cpu" {
+  type    = string
+  default = "1"
+}
+variable "memory" {
+  type    = string
+  default = "512Mi"
+}
+variable "cpu_idle" {
+  type    = bool
+  default = true
+}
 variable "env_vars" { 
   type = list(object({ name = string, value = string }))
   default = [] 
@@ -19,4 +53,7 @@ variable "secrets" {
   type = list(object({ name = string, secret_id = string }))
   default = [] 
 }
-variable "is_public" { type = bool; default = false }
+variable "is_public" {
+  type    = bool
+  default = false
+}
